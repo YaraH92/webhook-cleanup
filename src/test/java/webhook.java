b80f527ea23d7f2d8718136c6e745a164a3245d8
YaraH92 <yara.haddad@experitest.com>
2019-06-27 18:32:42 +0300

    webhook cleanup seetest commit

--- src/test/java/webhook.java
+++ src/test/java/webhook.java
@@ -1,72 +1,48 @@
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
-import io.appium.java_client.remote.AndroidMobileCapabilityType;
-import io.appium.java_client.remote.MobileBrowserType;
-import io.appium.java_client.remote.MobileCapabilityType;
+import com.experitest.client.Client;
+import com.experitest.client.GridClient;
 import org.apache.http.HttpHeaders;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.ScreenOrientation;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
-import java.net.URL;
+
 
 public class webhook {
 
     private String accessKey = "eyJ4cC51IjoyLCJ4cC5wIjoxLCJ4cC5tIjoiTVRVMk1UWTBNalkyTlRjMk1BIiwiYWxnIjoiSFMyNTYifQ.eyJleHAiOjE4NzcwMDI2NjUsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.B3ptPrCxbg06_BubE9SQUNzWvDa0iNepN7bqOD0w4C4";
-    protected AndroidDriver<AndroidElement> driver = null;
-    DesiredCapabilities dc = new DesiredCapabilities();
+
+    protected Client client = null;
+    protected GridClient grid = null;
+    private String host = "https://mastercloud.experitest.com";
     private String uid=System.getenv("deviceID");
     private String status="failed";
 
     @Before
-    public void setUp() throws MalformedURLException {
-        dc.setCapability("testName", "Quick Start Android Native Demo");
-        dc.setCapability("accessKey", accessKey);
-        dc.setCapability("deviceQuery", "@serialnumber='"+uid+"'");
-        dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.experitest.ExperiBank");
-        dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".LoginActivity");
-       // dc.setBrowserName(MobileBrowserType.CHROMIUM);
-        driver = new AndroidDriver<>(new URL("https://mastercloud.experitest.com/wd/hub"), dc);
+    public void setUp()  {
+
+        grid = new GridClient(accessKey, host);
+        client = grid.lockDeviceForExecution("Quick Start seetest iOS NATIVE Demo", "@serial='", 10, 50000);
+        client.setReporter("xml", "", "Quick Start seetest iOS Native Demo");
     }
 
     @Test
     public void quickStartAndroidNativeDemo() {
-//        driver.rotate(ScreenOrientation.PORTRAIT);
-//        driver.get("https://www.google.com");
-//        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.name("q")));
-//        WebElement searchBar = driver.findElement(By.name("q"));
-//        searchBar.sendKeys("Experitest");
-//        driver.findElement(By.xpath("//*[@css='BUTTON.Tg7LZd']")).click();
-        driver.rotate(ScreenOrientation.PORTRAIT);
-        driver.findElement(By.xpath("//*[@id='usernameTextField']")).sendKeys("company");
-        driver.hideKeyboard();
-        driver.findElement(By.xpath("//*[@id='passwordTextField']")).sendKeys("company");
-        driver.findElement(By.xpath("//*[@id='loginButton']")).click();
-        driver.findElement(By.xpath("//*[@id='makePaymentButton']")).click();
-        driver.findElement(By.xpath("//*[@id='phoneTextField']")).sendKeys("0541234567");
-        driver.findElement(By.xpath("//*[@id='nameTextField']")).sendKeys("Jon Snow");
-        driver.findElement(By.xpath("//*[@id='amountTextField']")).sendKeys("50");
-        driver.findElement(By.xpath("//*[@id='countryButton']")).click();
-        driver.findElement(By.xpath("//*[@text='Switzerland']")).click();
-        driver.findElement(By.xpath("//*[@id='sendPaymentButton']")).click();
-        driver.findElement(By.xpath("//*[@text='Yes']")).click();
+        client.run("adb shell pm list packages");
+        client.getInstalledApplications();
         status="passed";
     }
 
     @After
     public void tearDown() {
         sendResponseToCloud();
-        System.out.println("Report URL: "+ driver.getCapabilities().getCapability("reportUrl"));
-        driver.quit();
+        // Generates a report of the test case.
+        // For more information - https://docs.experitest.com/display/public/SA/Report+Of+Executed+Test
+        client.generateReport(false);
+        // Releases the client so that other clients can approach the agent in the near future.
+        client.releaseClient();
     }
 
     private void sendResponseToCloud() {
