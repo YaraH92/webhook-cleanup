ae1a5268fd4eb68148ac609bc751b9058b74c9b5
YaraH92 <yara.haddad@experitest.com>
2019-06-30 11:08:22 +0300

    webhook cleanup seetest commit

--- src/test/java/webhook.java
+++ src/test/java/webhook.java
@@ -1,63 +1,66 @@
-import com.experitest.client.Client;
-import com.experitest.client.GridClient;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.IOSElement;
+import io.appium.java_client.remote.IOSMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.apache.http.HttpHeaders;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class webhook {
 
-     private String accessKey = "eyJ4cC51IjoyLCJ4cC5wIjoxLCJ4cC5tIjoiTVRVMk1UWTBNalkyTlRjMk1BIiwiYWxnIjoiSFMyNTYifQ.eyJleHAiOjE4NzcwMDI2NjUsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.B3ptPrCxbg06_BubE9SQUNzWvDa0iNepN7bqOD0w4C4";
- //   private String accessKey = "eyJ4cC51IjoxMzYsInhwLnAiOjIsInhwLm0iOiJNVFUwTlRZMU1EYzRNVEF4T1EiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE4NjEwMTA3ODEsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.5D_cBCiQ77GeL8_7p0m2IdFPaJr4ieGqtRtprKDv1PI";
+    private String accessKey = "eyJ4cC51IjoyLCJ4cC5wIjoxLCJ4cC5tIjoiTVRVMk1UWTBNalkyTlRjMk1BIiwiYWxnIjoiSFMyNTYifQ.eyJleHAiOjE4NzcwMDI2NjUsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.B3ptPrCxbg06_BubE9SQUNzWvDa0iNepN7bqOD0w4C4";
+  //  private String accessKey = "eyJ4cC51IjoxNDAsInhwLnAiOjgyLCJ4cC5tIjoiTVRVME5qUXhOemd4TnpJd05BIiwiYWxnIjoiSFMyNTYifQ.eyJleHAiOjE4NjE3Nzc4MTcsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.uYd6r4Ax5FfuemmDlb4CUF1xoXDixXcbzqRMIvJ0KT4";
 
-    protected Client client = null;
-    protected GridClient grid = null;
-    private String host = "https://mastercloud.experitest.com";
+    protected IOSDriver<IOSElement> driver = null;
+    DesiredCapabilities dc = new DesiredCapabilities();
     private String uid=System.getenv("deviceID");
- //   private String uid="CVH7N15B04005855";
+  //  private String uid="CVH7N15B04005855";
     private String status="failed";
 
     @Before
-    public void setUp()  {
+    public void setUp() throws MalformedURLException {
 
-        grid = new GridClient(accessKey, host);
-        client = grid.lockDeviceForExecution("web hook cleanup", "@serialnumber='"+uid+"'", 10, 50000);
-        client.setReporter("xml", "", "web hook cleanup");
+        dc.setCapability("testName", "web hook cleanup");
+        dc.setCapability("accessKey", accessKey);
+        dc.setCapability("deviceQuery", "@serialnumber='"+uid+"'");
+      //  dc.setCapability(MobileCapabilityType.APP, "cloud:com.experitest.ExperiBank");
+        dc.setCapability(IOSMobileCapabilityType.BUNDLE_ID, "com.experitest.ExperiBank");
+        driver = new IOSDriver<>(new URL("https://mastercloud.experitest.com/wd/hub"), dc);
     }
 
     @Test
     public void quickStartAndroidNativeDemo() {
-////        client.run("adb shell pm list packages");
-////        client.getInstalledApplications();
-//          client.reboot(120 * 1000);
-        client.launch("com.experitest.ExperiBank", true, true);
-        client.elementSendText("NATIVE", "placeholder=Username", 0, "company");
-        client.elementSendText("NATIVE", "placeholder=Password", 0, "company");
-        client.click("NATIVE", "text=Login", 0, 1);
-        client.click("NATIVE", "text=Make Payment", 0, 1);
-        client.elementSendText("NATIVE", "placeholder=Phone", 0, "1234567");
-        client.elementSendText("NATIVE", "placeholder=Name", 0, "Jon Snow");
-        client.elementSendText("NATIVE", "placeholder=Amount", 0, "50");
-        client.click("NATIVE", "placeholder=Country", 0, 1);
-        client.click("NATIVE", "text=Select", 0, 1);
-        client.click("NATIVE", "text=Switzerland", 0, 1);
-        client.click("NATIVE", "text=Send Payment", 0, 1);
-        client.click("NATIVE", "text=Yes", 0, 1);
-        client.click("NATIVE", "text=Logout", 0, 1);
+        driver.rotate(ScreenOrientation.PORTRAIT);
+        driver.findElement(By.xpath("//*[@id='usernameTextField']")).sendKeys("company");
+        driver.hideKeyboard();
+        driver.findElement(By.xpath("//*[@id='passwordTextField']")).sendKeys("company");
+        driver.findElement(By.xpath("//*[@id='loginButton']")).click();
+        driver.findElement(By.xpath("//*[@id='makePaymentButton']")).click();
+        driver.findElement(By.xpath("//*[@id='phoneTextField']")).sendKeys("0541234567");
+        driver.findElement(By.xpath("//*[@id='nameTextField']")).sendKeys("Jon Snow");
+        driver.findElement(By.xpath("//*[@id='amountTextField']")).sendKeys("50");
+        driver.findElement(By.xpath("//*[@id='countryButton']")).click();
+        driver.findElement(By.xpath("//*[@id='Switzerland']")).click();
+        driver.findElement(By.xpath("//*[@id='sendPaymentButton']")).click();
+        driver.findElement(By.xpath("//*[@id='Yes']")).click();
         status="passed";
     }
 
     @After
     public void tearDown() {
-        client.generateReport(false);
         sendResponseToCloud();
-        // Generates a report of the test case.
-        // For more information - https://docs.experitest.com/display/public/SA/Report+Of+Executed+Test
-        // Releases the client so that other clients can approach the agent in the near future.
-//        client.releaseClient();
+        System.out.println("Report URL: "+ driver.getCapabilities().getCapability("reportUrl"));
+        driver.quit();
     }
 
     private void sendResponseToCloud() {
