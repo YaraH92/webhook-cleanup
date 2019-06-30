e51d16c0cdedb26f4dd5770133d223ce72da2915
YaraH92 <yara.haddad@experitest.com>
2019-06-30 10:39:59 +0300

    webhook cleanup seetest commit

--- src/test/java/webhook.java
+++ src/test/java/webhook.java
@@ -10,29 +10,29 @@ import org.junit.*;
 
 public class webhook {
 
-      private String accessKey = "eyJ4cC51IjoyLCJ4cC5wIjoxLCJ4cC5tIjoiTVRVMk1UWTBNalkyTlRjMk1BIiwiYWxnIjoiSFMyNTYifQ.eyJleHAiOjE4NzcwMDI2NjUsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.B3ptPrCxbg06_BubE9SQUNzWvDa0iNepN7bqOD0w4C4";
- //   private String accessKey = "eyJ4cC51IjoxNDAsInhwLnAiOjgyLCJ4cC5tIjoiTVRVME5qUXhOemd4TnpJd05BIiwiYWxnIjoiSFMyNTYifQ.eyJleHAiOjE4NjE3Nzc4MTcsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.uYd6r4Ax5FfuemmDlb4CUF1xoXDixXcbzqRMIvJ0KT4";
+     private String accessKey = "eyJ4cC51IjoyLCJ4cC5wIjoxLCJ4cC5tIjoiTVRVMk1UWTBNalkyTlRjMk1BIiwiYWxnIjoiSFMyNTYifQ.eyJleHAiOjE4NzcwMDI2NjUsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.B3ptPrCxbg06_BubE9SQUNzWvDa0iNepN7bqOD0w4C4";
+ //   private String accessKey = "eyJ4cC51IjoxMzYsInhwLnAiOjIsInhwLm0iOiJNVFUwTlRZMU1EYzRNVEF4T1EiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE4NjEwMTA3ODEsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.5D_cBCiQ77GeL8_7p0m2IdFPaJr4ieGqtRtprKDv1PI";
 
     protected Client client = null;
     protected GridClient grid = null;
     private String host = "https://mastercloud.experitest.com";
     private String uid=System.getenv("deviceID");
-//    private String uid="CVH7N15B04005855";
+ //   private String uid="CVH7N15B04005855";
     private String status="failed";
 
     @Before
     public void setUp()  {
 
         grid = new GridClient(accessKey, host);
-        client = grid.lockDeviceForExecution("Quick Start seetest iOS NATIVE Demo", "@serialnumber='"+uid+"'", 10, 50000);
-        client.setReporter("xml", "", "Quick Start seetest iOS Native Demo");
+        client = grid.lockDeviceForExecution("web hook cleanup", "@serialnumber='"+uid+"'", 10, 50000);
+        client.setReporter("xml", "", "web hook cleanup");
     }
 
     @Test
     public void quickStartAndroidNativeDemo() {
-//        client.run("adb shell pm list packages");
-//        client.getInstalledApplications();
-//        client.reboot(120 * 1000);
+////        client.run("adb shell pm list packages");
+////        client.getInstalledApplications();
+//          client.reboot(120 * 1000);
         client.launch("com.experitest.ExperiBank", true, true);
         client.elementSendText("NATIVE", "placeholder=Username", 0, "company");
         client.elementSendText("NATIVE", "placeholder=Password", 0, "company");
@@ -52,10 +52,10 @@ public class webhook {
 
     @After
     public void tearDown() {
+        client.generateReport(false);
         sendResponseToCloud();
         // Generates a report of the test case.
         // For more information - https://docs.experitest.com/display/public/SA/Report+Of+Executed+Test
-       client.generateReport(false);
         // Releases the client so that other clients can approach the agent in the near future.
 //        client.releaseClient();
     }
